8fb16d2a320eae9ce1886dc579ecba7316558afd
Haoran <a@h88i.com>
2020-09-30 16:10:01 +0800

    Create exp 2 .cpp

---
+++ exp 2 .cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+typedef struct LNode{
+	int data;
+	struct LNode  * next;
+
+} LNode, * LinkList;
+
+int InitList(LinkList &L) {
+	L = new LNode;
+	L->next = Null;
+	return 0;
+
+}
+
+int ReadList(int n, LinkList &L) {
+	LNode * p;
+	p = L->next;
+
+	for (int i = 1;i <= n;i++) {
+		scanf("%d",p->data );
+		p = p->next;
+	}
+	return 0;
+}
+
+int main() {
+	LinkList List;
+	InitList(List);
+	int length;// the length of the list t o read;
+	printf("Please input the length of the list");
+
+	scanf("%d", &length);
+	printf("input the content of the list");
+	ReadList(length, List);
+
+
+
+}
