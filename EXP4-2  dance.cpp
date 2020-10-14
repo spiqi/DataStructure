59054dc3c35edc5f013df7e70f54d4d3e0f83832
Haoran <a@h88i.com>
2020-10-14 16:43:00 +0800

    Update EXP4-2  dance.cpp

--- EXP4-2  dance.cpp
+++ EXP4-2  dance.cpp
@@ -8,6 +8,7 @@ typedef struct {
 int Init(SqQueue & Q) {
 	Q.base = new int[MAX];
 	if (!Q.base) exit(OVERFLOW); //溢出
+	Q.front = Q.rear = 0;
 	return 0;
 }
 int QueueLength(SqQueue Q) {
@@ -23,7 +24,7 @@ int EnQueue(SqQueue & Q, int e)
 	return 0;
 }  //入队
 int DeQueue(SqQueue & Q) {  //循环队列的出队
-	
+
 	if (Q.front == Q.rear) return 1;//队空
 	int e = Q.base[Q.front];
 	Q.front = (Q.front + 1) % MAX;
@@ -34,15 +35,31 @@ int GetHead(SqQueue Q) {
 }
 
 int main() {
-	SqQueue  sq1,sq2 ;
-	Init(sq1);
-	Init(sq2);
+	SqQueue  sq1;
+	Init(sq1); // 男队初始化
+	SqQueue sq2;
+	Init(sq2);  //女队
 	std::cout << "Please input the number of males and females\n";
 	int a, b;
-	std::cin >> a >> b;
-	for (int i = 0;i < a;i++) EnQueue(sq1, i);
-	for (int i = 0;i < b;i++) EnQueue(sq2, i);//读入女队
-	std::cout << "Please input the number of the music";
+	std::cin >> a >> b;// 读入男 女 
+	for (int i = 1; i <=a; i++) EnQueue(sq1, i);
+
+	for (int i = 1; i <=b; i++) EnQueue(sq2, i);//读入女队
+	std::cout << "Please input the number of the music\n";//读入歌曲的数量
 	std::cin >> a;
-	for (int i = 0;i < a;i++) {}
+	std::cout << "Input each music will accept the number of dancers"<<std::endl;//输入每首音乐需要的舞伴数目
+	std::cin >> b;//
+	for (int i = 0; i < a; i++) {
+		std::cout <<std::endl<< "Song:" << i << "\n";
+		for (int j = 0; j < b; j++) {
+			int s = DeQueue(sq1);
+			std::cout << s; //nan出队
+			EnQueue(sq1, s);
+			s = DeQueue(sq2);
+			std::cout << " " << s << std::endl; // nv 出队
+			EnQueue(sq2, s);//女入队
+		}
+		
+	}
+std::cin >> a; //test for pause 
 }
