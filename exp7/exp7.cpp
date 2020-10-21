1ca5fcdc319392144a5c5aeee6bebf0da9b849b5
Haoran <a@h88i.com>
2020-10-21 16:11:10 +0800

    Update exp7.cpp

--- exp7/exp7.cpp
+++ exp7/exp7.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
-typedef struct tree{
- int data;
- struct tree *lchild,*rchild;
- }tree;
+typedef struct BitNode {
+	int data;
+	struct BitNode *lchild, *rchild;
+}BitNode, *tree;
+void CreateBiTree(tree &T) {
+	char ch;
+	std::cin >> ch;
+	if (ch == '#') T = NULL;
+	else {
+		T = new BitNode;
+		T->data = ch;
+		CreateBiTree(T->lchild);
+		CreateBiTree(T->rchild);//先序遍历 genzuoy
+
+	}
+}
