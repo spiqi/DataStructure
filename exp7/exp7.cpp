4f84e8fad696f474579144b436dedfa190fd8a6e
Haoran <a@h88i.com>
2020-10-28 17:08:04 +0800

    Update exp7.cpp

--- exp7/exp7.cpp
+++ exp7/exp7.cpp
@@ -1,38 +1,35 @@
 #include<iostream>
 #define ERROR 114514
 #define MAX 19892019
-
+const int bignum= 1145;
 typedef struct BitNode {
 	char data;
 	struct BitNode *lchild, *rchild;
-}BitNode; //用BitNode* 代替BiTree
-typedef struct { //栈结构定义部分
-	BitNode * base;
-	BitNode * top;
-	int stacksize;
-} SqStack;
-int InitStack(SqStack &S) { //顺序栈
-	S.base = new BitNode[MAX];
-	if (!S.base) exit(OVERFLOW);// 存储分配失败
-	S.top = S.base;
-	S.stacksize = MAX;
+}BitNode ; //用BitNode* 代替BiTree
+typedef struct StackNode
+{
+	BitNode * data[bignum];
+	int top;
+	
+}StackNode;
+int InitStack(StackNode  &S) {
+	S.top = 0;
 	return 0;
 }
-int Push(SqStack &S, BitNode * e) {
-	if (S.top - S.base == S.stacksize) return ERROR;// stack if full
-	*(S.top)++ = e;
-	return 0;
-}
-int Pop(SqStack & S, BitNode * & e) {
-	if (S.top == S.base) return ERROR;
-	e = --S.top;
-	return 0;
-}
-bool StackEmpty(SqStack S) {
-	if (S.top == S.base)return true;//为空
-	else return false;//不为空
-}//栈结构定义结束
+int Push(StackNode S, BitNode * e) {
+	StackNode *p = new StackNode;
+	S.data[++(S.top)]=e;
 
+	return 0;
+}
+int Pop(StackNode S, BitNode * &e) {
+	e = S.data[(S.top)--];
+	return 0;
+}
+bool StackEmpty(StackNode S) {
+	if (S.top==0) return true;
+	return false;
+}
 
 void CreateBiTree(BitNode* &T) {
 	char ch;
@@ -48,7 +45,7 @@ void CreateBiTree(BitNode* &T) {
 
 void InOrderTraver2(BitNode *T) {
 	//中序遍历二叉树的Fei递归算法
-	SqStack St;
+	StackNode  St;
 	InitStack(St);
 	BitNode *p = T;
 	BitNode * q = new BitNode;
@@ -57,9 +54,10 @@ void InOrderTraver2(BitNode *T) {
 			Push(St, p);
 			p = p->lchild;
 		}
-		else { Pop(St, q);
-		std::cout << q->data;
-		p = q->rchild;
+		else {
+			Pop(St, q);
+			std::cout << q->data;
+			p = q->rchild;
 
 		}
 	}
@@ -73,15 +71,16 @@ void InOrderTraverse(BitNode* T) { //中序遍历的递归算法
 	}
 }
 void PreOrderTraverse(BitNode* T) { //xian序遍历的递归算法
-	if (T) {std::cout << T->data;
+	if (T) {
+		std::cout << T->data;
 		PreOrderTraverse(T->lchild);
-		
+
 		PreOrderTraverse(T->rchild);
 	}
 }
 void PostOrderTraverse(BitNode* T) { //xian序遍历的递归算法
 	if (T) {
-		
+
 		PostOrderTraverse(T->lchild);
 		PostOrderTraverse(T->rchild);
 		std::cout << T->data;
@@ -96,20 +95,21 @@ int Depth(BitNode * T) {
 		else return n + 1;
 	}
 }
-int main() {
-	BitNode * tree;
-	CreateBiTree(tree);
-	std::cout << std::endl << std::endl << "PreOrder Is:" << std::endl;
-	PreOrderTraverse(tree);
-	std::cout << std::endl <<std::endl<< "InOrder Is :" << std::endl;
-	InOrderTraverse(tree);
-	std::cout << std::endl << "PostOrder Is:" << std::endl ;
-	PostOrderTraverse(tree);
-	std::cout << std::endl << std::endl << "Depth Is:"<<Depth(tree) << std::endl << std::endl;
-	std::cout << std::endl << std::endl << "test for the InOrderTraverse";
+
+	int main() {while(1){		BitNode * tree;
+		CreateBiTree(tree);
+		std::cout << std::endl << std::endl << "PreOrder Is:" << std::endl;
+		PreOrderTraverse(tree);
+		std::cout << std::endl << std::endl << "InOrder Is :" << std::endl;
+		InOrderTraverse(tree);
+		std::cout << std::endl << "PostOrder Is:" << std::endl;
+		PostOrderTraverse(tree);
+		std::cout << std::endl << std::endl << "Depth Is:" << Depth(tree) << std::endl << std::endl;
+		std::cout << std::endl << std::endl << "test for the InOrderTraverse";
 		InOrderTraver2(tree);
-		std::cout<< std::endl << std::endl;
-
-	return 0;
-
+		std::cout << std::endl << std::endl;
 }
+
+		return 0;
+
+	} 
